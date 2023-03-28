63de8a740f6a28679a82aa1e6a39a83c01aa275b
AdrianVargas23 <adrianpatata23@live.com>
2023-03-27 19:22:03 -0700

    Primer demo

--- main.py
+++ main.py
@@ -2,20 +2,6 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
-import pyrebase
-
-firebaseConfig={
-    'apiKey': "AIzaSyCjvh2Ka6B1cIdDWgOzANa1SSG8sCs3P0c",
-    'authDomain': "mike-sbikes.firebaseapp.com",
-    'projectId': "mike-sbikes",
-    'storageBucket': "mike-sbikes.appspot.com",
-    'messagingSenderId': "578901172163",
-    'appId': "1:578901172163:web:f9d62d14d3b42c690d2776",
-    'measurementId': "G-ZGYHQ01XQ1"}
-
-firebase=pyrebase.initialize_app(firebaseConfig)
-
-auth=firebase.auth()
 
 
 class Login(QDialog):
@@ -26,21 +12,21 @@ class Login(QDialog):
         self.password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.createaccButton.clicked.connect(self.gotocreate)
 
-
     def loginfunction(self):
         email = self.email.text()
         password = self.password.text()
-        print("Si entraste perrillo")
+        print("Si entraste perrillo", email, password)
+        self.gotoHomescreen()  # Call the method to go to HomeScreen
 
     def gotocreate(self):
-        createacc=CreateAcc()
+        createacc = CreateAcc()
         widget.addWidget(createacc)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
     def gotoHomescreen(self):
         gotoHomeScreen = HomeScreen()
         widget.addWidget(gotoHomeScreen)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
+        widget.setCurrentIndex(widget.currentIndex() + 2)
 
 
 class CreateAcc(QDialog):
@@ -52,39 +38,37 @@ class CreateAcc(QDialog):
         self.confirmPassword.setEchoMode(QtWidgets.QLineEdit.Password)
 
 
+
     def createaccfunction(self):
         email=self.email.text()
         if self.password.text()==self.confirmPassword.text():
             password=self.password.text()
-            auth.create_user_with_email_and_password(email, password)
-            login=Login()
-            widget.addWidget(login)
-            widget.setCurrentIndex(widget.currentIndex()+1)
-
+            print("Succesfully created acc with email: ", email, " and password: ", password)
+            self.close()  # Close the current window after creating account
+            widget.setCurrentIndex(0)  # Return to Login window
 
 
 class HomeScreen(QDialog):
     def __init__(self):
         super(HomeScreen, self).__init__()
         loadUi("homescreen.ui", self)
-        self.acceptButton.clicked.connect(self.rentBike)
+        #self.acceptButton.clicked.connect(self.rentBike)
 
     def rentBike(self):
-        name = self.nameLineEdit.text()
-        phone = self.phoneLineEdit.text()
-        bikeID = self.bikeidComboBox.text()
-        date = self.dateEdit.text()
-        time = self.timeComboBox.text()
-        print(name + ", " + phone + ", " + bikeID + ", " + date + ", " + time)
+        print("hola")
+        #name = self.nameLineEdit.text()
+        #phone = self.phoneLineEdit.text()
+        #bikeID = self.bikeidComboBox.text()
+        #date = self.dateEdit.text()
+        #time = self.timeComboBox.text()
+        #print(name + ", " + phone + ", " + bikeID + ", " + date + ", " + time)
 
 
 app = QApplication(sys.argv)
 mainwindow = Login()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
-widget.setFixedWidth(480)
-widget.setFixedHeight(620)
+widget.setFixedWidth(720)
+widget.setFixedHeight(720)
 widget.show()
 app.exec_()
-
-#comentario pedorro nomas para asegurarme de hacer el commit nuevo
